9b6ee9df3961d5d7f8cf5dfa722d683664527077
Dhaval Solanki <dhavalas24@gmail.com>
2024-07-27 18:06:37 +0530

    jquery

---
+++ lecture 1/lecture_1.js
@@ -0,0 +1,36 @@
+/* Syntax of jQuery */
+
+// $(document).ready(function(){
+//     $('#id').click(function(){
+//         $('.class').hide()
+//     })
+// })
+
+/* Syntax of jQuery shortcut */
+
+// $(function(){
+//     $('#id').click(function(){
+//         $('.class').hide()
+//     })
+// })
+
+/* method */
+
+// Hide /show/toggle Method
+
+// $(function(){
+//     $('#hide').click(function(){
+//         $('#firstHeading').hide()
+//     })
+// })
+
+// $(function(){
+//     $('#show').click(function(){
+//         $('#firstHeading').show()
+//     })
+// })
+// $(function(){
+//     $('#Toggle').click(function(){
+//         $('#firstHeading').toggle()
+//     })
+// })
