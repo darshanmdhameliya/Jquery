ab116176b7b3794f8a220253c0521b26974c4267
Dhaval Solanki <dhavalas24@gmail.com>
2024-07-31 18:17:28 +0530

    lecture_2

---
+++ lecture 2 HTML/lecture2_HTML.js
@@ -0,0 +1,111 @@
+/* jQuery HTML */
+
+/* get method */
+// $(function () {
+//   $(".showtext").click(function () {
+//     alert($(".heading").text());
+//   });
+//   $(".showhtml").click(function () {
+//     alert($(".heading").html());
+//   });
+//   $(".showvalue").click(function () {
+//     alert($(".input").val());
+//   });
+// });
+
+/*set method */
+// $(function () {
+//   $(".settext").click(function () {
+//     $(".heading").text("set heading");
+//   });
+//   $(".sethtml").click(function () {
+//     $(".heading").html("<b>Hello world!</b>");
+//   });
+//   $(".setvalue").click(function () {
+//     $(".input").val("darshan");
+//   });
+// });
+
+/* Add method */
+
+/* append = content pachi, prepend = content pehla , after = tag pachi , before = tag pehla */
+
+// $(function(){
+//     $('.append').click(function(){
+//         $('.heading').append('hello word')
+//     })
+// })
+// $(function(){
+//     $('.after').click(function(){
+//         $('.heading').after('hello word')
+//     })
+// })
+// $(function(){
+//     $('.prepend').click(function(){
+//         $('.heading').prepend('hello word')
+//     })
+// })
+// $(function(){
+//     $('.before').click(function(){
+//         $('.heading').before('hello word')
+//     })
+// })
+
+/* Remove() method => badhu j remove kri nakhe tag and content*/
+
+// $(function () {
+//   $(".Remove").click(function () {
+//     $(".heading").remove();
+//   });
+// });
+
+/* empty() method => only content j remove kre*/
+// $(function () {
+//   $(".empty").click(function () {
+//     $(".heading").empty();
+//   });
+// });
+
+/* CSS classes */
+
+// addClass()
+// removeClass()
+// toggleClass()
+// css()
+
+// addClass()
+// $(function(){
+//     $('.addclass').click(function(){
+//         $('#heading').addClass('heading')
+//     })
+// })
+
+// removeClass()
+// $(function(){
+//     $('.removeclass').click(function(){
+//         $('#heading').removeClass('heading')
+//     })
+// })
+
+// toggleClass()
+// $(function(){
+//     $('.toggleclass').click(function(){
+//         $('#heading').toggleClass('heading')
+//     })
+// })
+
+// css()
+// $(function () {
+//   $(".css").click(function () {
+//     $("#heading").css("background-color", "yellow");
+//   });
+// });
+//multiple-css property
+// $(function () {
+//   $(".css").click(function () {
+//     $("#heading").css({
+//       "background-color": "yellow",
+//       color: "red",
+//     });
+//   });
+// });
